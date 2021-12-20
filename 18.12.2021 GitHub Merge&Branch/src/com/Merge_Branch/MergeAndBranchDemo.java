f99ece85f3fbd0a91fba81b811ab7a2827afbd55
user <user@user-PC>
2021-12-20 14:44:58 +0530

    by akash

---
+++ 18.12.2021 GitHub Merge&Branch/src/com/Merge_Branch/MergeAndBranchDemo.java
@@ -0,0 +1,14 @@
+package com.Merge_Branch;
+
+public class MergeAndBranchDemo {
+
+	public void  m1()
+	{
+		System.out.println("Method M1...");
+	}
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Helloo ");
+	}
+}
