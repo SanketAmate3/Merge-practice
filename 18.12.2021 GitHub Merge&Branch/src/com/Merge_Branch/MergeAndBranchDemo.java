7c72ecc1b28093cbd099e674c3e2de96921b91f6
Aakash2500 <96283100+Aakash2500@users.noreply.github.com>
2021-12-20 14:50:59 +0530

    Update MergeAndBranchDemo.java

--- 18.12.2021 GitHub Merge&Branch/src/com/Merge_Branch/MergeAndBranchDemo.java
+++ 18.12.2021 GitHub Merge&Branch/src/com/Merge_Branch/MergeAndBranchDemo.java
@@ -6,6 +6,10 @@ public class MergeAndBranchDemo {
 	{
 		System.out.println("Method M1...");
 	}
+	public void  m2()
+	{
+		System.out.println("Method M1...");
+	}
 	public static void main(String[] args) {
 		
 		
